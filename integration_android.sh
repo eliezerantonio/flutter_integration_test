e7a7067a8b84c19bee4bd7a4aa958b98d92bfaeb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2022-05-17 05:55:13 +0100

    automatinzando com test lab atraves do terminarl

--- integration_android.sh
+++ integration_android.sh
@@ -3,3 +3,15 @@ flutter build apk
 ./gradlew app:assembleAndroidTest
 ./gradlew app:assembleDebug -Ptarget=integration_test/app_test.dart
 popd
+
+gcloud firebase test android run --type instrumentation \
+  --app build/app/outputs/apk/debug/app-debug.apk \
+  --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+  --device-ids=Pixel2 \
+  --os-version-ids=29 \
+  --orientations=portrait \
+  --use-orchestrator \
+  --timeout 3m \
+  --results-bucket=gs://organize-993af.appspot.com \
+  --results-dir=tests/firebase
+
